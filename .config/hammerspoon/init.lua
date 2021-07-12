ad60d47d2da4a659c10412a62cda3fbb24bbf035
yto <yto@pixiv.co.jp>
2021-07-13 00:25:01 +0900

    update hammerspoon settings

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,13 +1,12 @@
 hs.loadSpoon("SpoonInstall")
 
+-- ShiftIt
 spoon.SpoonInstall.repos.ShiftIt = {
    url = "https://github.com/peterklijn/hammerspoon-shiftit",
    desc = "ShiftIt spoon repository",
    branch = "master",
 }
-
 spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
-
 spoon.ShiftIt:bindHotkeys({
    left = {{ 'ctrl', 'cmd', 'alt' }, 'left' },
    right = {{ 'ctrl', 'cmd', 'alt' }, 'right' },
@@ -20,3 +19,24 @@ spoon.ShiftIt:bindHotkeys({
    center = {{ 'ctrl', 'cmd', 'alt' }, 'c' },
    maximum = {{ 'ctrl', 'cmd', 'alt' }, 'm' }
 });
+
+-- Overwrite Slack and Discord Shortcuts
+local keybinds = {
+   -- 次の未読チャンネルに移動
+   hs.hotkey.new({"alt"}, "e", function()
+     hs.eventtap.keyStroke({"alt", "shift"}, "Down")
+   end),
+   -- 前の未読チャンネルに移動
+   hs.hotkey.new({"alt", "shift"}, "e", function()
+     hs.eventtap.keyStroke({"alt", "shift"}, "Up")
+   end)
+ }
+ local appWatcher = hs.application.watcher.new(function(name, eventType, app)
+   if eventType ~= hs.application.watcher.activated then return end
+   -- Slack または Discord を起動したときは enable, それ以外を起動したときは disable
+   local fnName = (name == "Slack" or name == "Discord") and "enable" or "disable"
+   for i, keybind in ipairs(keybinds) do
+     keybind[fnName](keybind)
+   end
+ end)
+ appWatcher:start()
